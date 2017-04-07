ffec12168ec17050a4384315d3337d1ed3c82455
mike-houghton <mike@feefo.com>
2017-04-07 10:58:19 +0100

    latest

--- testNLP/nlp.py
+++ testNLP/nlp.py
@@ -1,6 +1,7 @@
-import nltk
-from nltk.classify import NaiveBayesClassifier
-from nltk.tokenize import sent_tokenize, word_tokenize
+# import nltk
+# from nltk.classify import NaiveBayesClassifier
+from textblob.classifiers import NaiveBayesClassifier
+# from nltk.tokenize import sent_tokenize, word_tokenize
 
 # https://www.youtube.com/watch?v=IMKweOTFjXw
 positive = []
@@ -11,6 +12,7 @@ positive_test = []
 negative_test= []
 
 all_trg_data = []
+word_features = []
 
 POSITIVE = "POSITIVE"
 NEGATIVE = "NEGATIVE"
@@ -42,22 +44,52 @@ def fill_data():
         for line in f:
             negative.append((line, NEGATIVE))
 
+    global positive_test
     positive_test = positive[:100]
-    positive_trg  = positive[100:]
+
+    global positive_trg
+    positive_trg = positive[100:]
+
+    global negative_test
     negative_test = negative[:100]
-    negative_trg  = negative[100:]
+
+    global negative_trg
+    negative_trg = negative[100:]
 
     for words, sentiment in positive_trg + negative_trg:
         filtered_words = [w.lower().strip('.').strip(',').strip(';').strip(';').strip(':').strip('"').strip('!').strip('?')
                           for w in words.split() if len(w) >=4]
         all_trg_data.append((filtered_words, sentiment))
 
+def get_just_words(data):
+    all_words = []
+    for(words, sentiment) in data:
+        all_words += words
+    return all_words;
+
+
+def extract_features(src_doc):
+    document_words = set(src_doc)
+    features = {}
+
+    for word in word_features:
+        features ['contains(%s)' % word] =  (word in document_words)
+
+    return features
+
+
 
 proc_review("negative.txt", "negReviews.txt")
 proc_review("positive.txt", "posReviews.txt")
 
 fill_data()
-print (all_trg_data)
+# word_features = nltk.FreqDist( get_just_words(all_trg_data) ).keys()
+#
+# print (positive_trg[1])
+#
+# print (extract_features(positive_trg[1]))
 
+print (all_trg_data[1])
+classifier = NaiveBayesClassifier.train(all_trg_data[1])
 
-
+classifier.show_most_informative_features()
